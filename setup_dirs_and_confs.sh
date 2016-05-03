10c3e5d2deafff14943dfd425362f3a8da52516a
Thomas Taylor-Hall <tjt12@ic.ac.uk>
2016-05-03 21:30:07 +0100

    #MakeDebuggingGreatAgain
    Can now use console-style navigation in jdb, and attach to the vm remotely.

--- setup_dirs_and_confs.sh
+++ setup_dirs_and_confs.sh
@@ -18,6 +18,7 @@ do
 
   port=$((${i}+18445))
   rpcport=$((${i}+20222))
+  dbgport=$((${i}+30111))
   args="-printtoconsole -datadir=$PREFIX/$NEWDIR/data -regtest -daemon -printtoconsole -listen -port=$port -rpcport=$rpcport -rpcuser=test -rpcpassword=test -acceptnonstdtxn"
   com="$LOTTERYCOINLOC/src/bitcoind $args"
   
@@ -29,8 +30,16 @@ do
   echo "$LOTTERYCOINLOC/src/bitcoin-cli -regtest -port=$port -rpcport=$rpcport -rpcuser=test -rpcpassword=test -whitelist=127.0.0.1 \$@" > bitcoin_command.sh
   chmod +x bitcoin_command.sh
 
+  echo "javac -cp slf4j-simple-1.7.16.jar:bitcoinj-core-0.14-SNAPSHOT-bundled.jar -g LotteryEntry.java" > debug_app.sh
+  echo "java -cp slf4j-simple-1.7.16.jar:bitcoinj-core-0.14-SNAPSHOT-bundled.jar:app.jar:. -Xdebug -agentlib:jdwp=transport=dt_socket,address=127.0.0.1:$dbgport,server=y,suspend=y LotteryEntry regtest $port" >> debug_app.sh
+
+  echo "java -cp .:jline-1.0.jar:/usr/lib/jvm/java-1.8.0-openjdk-amd64/lib/tools.jar jline.ConsoleRunner com.sun.tools.example.debug.tty.TTY -sourcepath /home/tjt12/bitcoinj-lotterycoin/core/src/main/java:. -attach 127.0.0.1:$dbgport" > attach_debugger.sh
+  
+  chmod +x debug_app.sh  
+  chmod +x attach_debugger.sh  
+
   cd ../
-  cp slf4j-simple-1.7.16.jar ~/bitcoinj-lotterycoin/core/target/bitcoinj-core-0.14-SNAPSHOT-bundled.jar LotteryEntry.java build_app.sh debug_app.sh $NEWDIR
+  cp jline-1.0.jar slf4j-simple-1.7.16.jar ~/bitcoinj-lotterycoin/core/target/bitcoinj-core-0.14-SNAPSHOT-bundled.jar LotteryEntry.java build_app.sh $NEWDIR
   echo "$NEWDIR created."
 
   echo `cat run_app.sh` "$port" > $NEWDIR/run_app.sh
